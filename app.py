78bc9ee04846b0c80c97386ad4b190df4949dc9b
Andersooonnn <andersoncatu@gmail.com>
2026-07-25 10:07:18 -0300

    Refatorado o modulo de deposito para blueprint, testado

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from routes.painel import painel_bp
 from routes.buscas import pesquisa_bp
 from routes.cadastro import cadastro_bp
 from routes.relatorio import relatorio_bp
+from routes.estoque.deposito import deposito_bp
 from routes.dados import lista , armazen
 
 
@@ -12,42 +13,6 @@ app = Flask(__name__)
 app.secret_key = 'Meucodigoshow'
 
 
-@app.route('/deposito', methods=('GET','POST'))
-def deposito():
-    if 'usuario_logado' not in session:
-        return redirect(url_for('login.login'))
-    if request.method == 'POST': 
-        pesquisa = False
-        produto = request.form['txtpesquisaa'].strip().lower()
-        for analise in lista: 
-            if analise['produto'] == produto: 
-                session['produto'] = produto
-                pesquisa = True 
-                return redirect(url_for('depositando'))
-
-        if pesquisa == False: 
-            return render_template('deposito.html',
-                            mensagem = "Produto não encontrado")
-        
-    else:
-        return render_template('deposito.html')
-
-#fazendo atualização =============================================================================
-@app.route('/depositando', methods=('POST','GET'))
-def depositando():
-    if 'usuario_logado' not in session:
-        return redirect(url_for('login.login'))
-    if request.method == 'POST':
-        produto = session['produto']
-        for analise in lista:
-            if analise['produto'] == produto:
-                
-                quantidade = int(request.form['numberquantidade'])
-                analise['quantidade'] += quantidade
-                session.pop('produto', None)
-                return redirect(url_for('relatorio'))
-
-    return render_template('depositando.html')
 
 
 
@@ -90,12 +55,12 @@ def retirando():
     return render_template('retirando.html')
 
 
-
 app.register_blueprint(login_bp)
 app.register_blueprint(painel_bp)
 app.register_blueprint(pesquisa_bp)
 app.register_blueprint(cadastro_bp)
 app.register_blueprint(relatorio_bp)
+app.register_blueprint(deposito_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ routes/estoque/deposito.py
@@ -0,0 +1,42 @@
+from flask import Blueprint, session , redirect , request , render_template , url_for
+from routes.dados import lista, armazen
+
+
+deposito_bp = Blueprint("deposito",__name__)
+
+@deposito_bp.route('/deposito', methods=('GET','POST'))
+def deposito():
+    if 'usuario_logado' not in session:
+        return redirect(url_for('login.login'))
+    if request.method == 'POST': 
+        pesquisa = False
+        produto = request.form['txtpesquisaa'].strip().lower()
+        for analise in lista: 
+            if analise['produto'] == produto: 
+                session['produto'] = produto
+                pesquisa = True 
+                return redirect(url_for('deposito.depositando'))
+
+        if pesquisa == False: 
+            return render_template('deposito.html',
+                            mensagem = "Produto não encontrado")
+        
+    else:
+        return render_template('deposito.html')
+
+
+@deposito_bp.route('/depositando', methods=('POST','GET'))
+def depositando():
+    if 'usuario_logado' not in session:
+        return redirect(url_for('login.login'))
+    if request.method == 'POST':
+        produto = session['produto']
+        for analise in lista:
+            if analise['produto'] == produto:
+                
+                quantidade = int(request.form['numberquantidade'])
+                analise['quantidade'] += quantidade
+                session.pop('produto', None)
+                return redirect(url_for('relatorio.relatorio'))
+
+    return render_template('depositando.html')
